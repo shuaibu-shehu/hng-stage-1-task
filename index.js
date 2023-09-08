1a7e581e541d7a1e4585ec58c1f9f216042d1e4e
shuaibu-shehu <shuaibushehukalifa@gmail.com>
2023-09-08 23:02:33 +0530

    converted to utc miliseconds time javascript

--- index.js
+++ index.js
@@ -16,32 +16,19 @@ let date = new Date();
 const currentDay = daysOFtheWeek[date.getUTCDay()];
 dayOFTheWeek.textContent = currentDay;
 
-function getTime() {
-  date = new Date();
-  let hrs = date.getUTCHours();
-  let mins = date.getUTCMinutes();
-  let secs = date.getUTCSeconds();
-  let ampm =document.querySelector(".ampm");
 
-  if(hrs>=12){
-    ampm.textContent='PM';
-  }
-  if(hrs>12){
-    hrs=hrs-12;
-  } else{
-    ampm.textContent='AM';
-  }
-  
-  document.querySelector(".hrs").textContent = formatToTwoDigits(hrs);
-  document.querySelector(".mins").textContent= formatToTwoDigits(mins);
-  document.querySelector(".secs").textContent = formatToTwoDigits(secs);
-}
 
 setInterval(() => {
-  getTime();
-}, 1000);
+  date = new Date();
+  const utcMilliseconds = Date.UTC(
+    date.getUTCFullYear(),
+    date.getUTCMonth(),
+    date.getUTCDate(),
+    date.getUTCHours(),
+    date.getUTCMinutes(),
+    date.getUTCSeconds(),
+    date.getUTCMilliseconds()
+  );
 
-// converts to two digits
-function formatToTwoDigits(number) {
-    return number.toString().padStart(2, '0');
-}
+  document.querySelector('[data-testid="currentUTCTime"]').textContent=utcMilliseconds;
+}, 1000);
